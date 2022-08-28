0377cf21e26d673be7ac9c8e82805139ac6f5475
Hiromu Yamada <hiromu@HiromunoMacBook-Air.local>
2022-08-28 12:14:55 +0900

    add env

---
+++ src/env.js
@@ -0,0 +1,9 @@
+// web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyCwkLeXuScZk3u7GY6ZkpdoMQUN47Ko9fU",
+    authDomain: "mysns-6b31e.firebaseapp.com",
+    projectId: "mysns-6b31e",
+    storageBucket: "mysns-6b31e.appspot.com",
+    messagingSenderId: "601522775758",
+    appId: "1:601522775758:web:89b44f1c970baed95ed29f"
+  };
